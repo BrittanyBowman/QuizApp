40b21b90ffb7e1c773715c787ac0222174393287
Brittany Bowman <britpetty@gmail.com>
2019-07-14 12:23:13 -0600

    Endpoint Testing challenge complete

--- src/Components/Functions/Functions.js
+++
@@ -1,6 +0,0 @@
-const mul = (a, b) => a * b;
-const sub = (a, b) => a - b;
-const div = (a, b) => a / b;
-const add = (a, b) => a + b;
-
-module.exports = { add, mul, sub, div };
---
+++ src/testing/server.js
@@ -0,0 +1,3 @@
+const app = require("./app");
+
+app.listen(4896, () => console.log("server starting on port 4896"));
